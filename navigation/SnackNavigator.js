f9e867ab140a820c9974e145577b5d6227828334
coachblockchain <150323702+coachblockchain@users.noreply.github.com>
2024-01-09 15:55:07 -0600

    Update SnackNavigator.js

--- navigation/SnackNavigator.js
+++ navigation/SnackNavigator.js
@@ -1,7 +1,70 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabNavigator from "./TabNavigator";
+import import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
 import StoryScreen from "../screens/StoryScreen";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="inicio" component={TabNavigator} />
+      <Stack.Screen name="PantallaDeHistoria" component={import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import StoryScreen from "../screens/StoryScreen";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="inicio" component={TabNavigator} />
+      <Stack.Screen name="PantallaDeHistoria" component={import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import StoryScreen from "../screens/StoryScreen";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="inicio" component={TabNavigator} />
+      <Stack.Screen name="PantallaDeHistoria" component={CreatePost} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator; from "../screens/CreatePost";
 
 const Stack = createStackNavigator();
 
